8d76e6f61aecfd7e81502f8d482b77b78174c492
jay9884 <68951034+jay9884@users.noreply.github.com>
2020-08-20 23:52:57 +0900

    add UI

---
+++ Assets/00. Script/UIManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager uimanager;
+    public GameObject Pause_Canvas;
+    public GameObject Inventory_Panel;
+    public GameObject WorldMap_Panel;
+    public GameObject Setting_Panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uimanager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void openPauseCanvas()
+    {
+        Pause_Canvas.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void closePauseCanvas()
+    {
+        Pause_Canvas.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void onInventory_Panel()
+    {
+        WorldMap_Panel.SetActive(false);
+        Setting_Panel.SetActive(false);
+        Inventory_Panel.SetActive(true);
+    }
+
+    public void onWorldMap_Panel()
+    {
+        Setting_Panel.SetActive(false);
+        Inventory_Panel.SetActive(false);
+        WorldMap_Panel.SetActive(true);
+    }
+
+    public void onSetting_Panel()
+    {
+        Inventory_Panel.SetActive(false);
+        WorldMap_Panel.SetActive(false);
+        Setting_Panel.SetActive(true);
+    }
+}
